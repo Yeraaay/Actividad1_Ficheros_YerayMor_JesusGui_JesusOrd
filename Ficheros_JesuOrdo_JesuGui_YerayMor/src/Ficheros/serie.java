16cbda2b3de347df46f659a57e4c338c7022971b
jesus <jesus.gomez1992@gmail.com>
2023-10-11 18:24:49 +0200

    clase serie y pelicula

--- Ficheros_JesuOrdo_JesuGui_YerayMor/src/Ficheros/serie.java
+++ Ficheros_JesuOrdo_JesuGui_YerayMor/src/Ficheros/serie.java
@@ -1,5 +1,31 @@
 package Ficheros;
 
+
 public class serie extends producto {
-//
+
+	public int temporadas=0;
+
+	public serie() {
+	    super();
+	    this.temporadas = temporadas; 
+	}
+	
+	
+	public  int getTemporadas() {
+		return temporadas;
+	}
+
+	public  void setTemporadas(int temporadas) {
+		temporadas = temporadas;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "serie [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+				+ "]";
+	}
+	
+	
+
 }
