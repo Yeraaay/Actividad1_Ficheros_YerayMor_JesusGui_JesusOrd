1647f43bce9842f63c516852e1b44d39dc740970
jesus <jesus.gomez1992@gmail.com>
2023-10-19 20:50:21 +0200

    COMMIT ESTRUCTURA

--- Ficheros_JesuOrdo_JesuGui_YerayMor/src/Ficheros/Genero.java
+++ Ficheros_JesuOrdo_JesuGui_YerayMor/src/Ficheros/Genero.java
@@ -1,5 +1,5 @@
 package Ficheros;
 
-public enum Genero {drama,acción,terror,comedia,suspense,bélica,aventura,documental,deportes,ciencia_ficción,animación,super_heroes
+public enum Genero {drama,acción,terror,comedia,suspense,bélica,aventura,documental,deportes,ciencia_ficción,animación,super_heroes, musical, familiar, infantil
 
 }
